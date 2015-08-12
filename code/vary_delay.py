8c140b1922f62d50480d80e5d6fb9213bcbeab0e
Terry Stewart <tcstewar@uwaterloo.ca>
2015-08-12 14:46:27 -0400

    changed fn

--- code/vary_delay.py
+++ code/vary_delay.py
@@ -3,7 +3,9 @@ from benchmark_min import AdaptiveControl
 import numpy as np
 rng = np.random.RandomState()
 
-for i in range(10):
+for i in range(40):
     AdaptiveControl().run(no_figs=True, T=20, seed=i, max_freq=1,
                 scale_add=1, delay=rng.uniform(0, 0.04),
+                filter=rng.uniform(0, 0.01),
+                noise=rng.uniform(0, 0.1),
                 adapt=True, data_dir='vary_delay')
